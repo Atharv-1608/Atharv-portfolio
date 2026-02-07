2f9ac046b5efd51b6545a35123a8b01d8703c186
atharv <datharv6@gmail.com>
2026-02-07 16:56:18 +0530

    first commit

---
+++ src/components/Landing.tsx
@@ -0,0 +1,24 @@
+export const Landing = ()=>{
+    return (
+        <div>
+            <div className="font-display font-extrabold pl-5 pt-5 text-xl">
+                AD <span className="text-[#e8743f] ">.</span>
+            </div>
+
+            <div className="flex flex-col justify-center items-center pt-10">
+                <div className="text-6xl  font-extrabold font-display">
+                    Atharv
+                </div>
+                <div className="text-6xl font-extrabold font-display text-[#e8743f]">
+                    Deorukhkar
+                </div>
+
+                 <div>
+                0s and 1s enthusiast , Engineer
+                </div>
+            </div>
+
+           
+        </div>
+    )
+}
